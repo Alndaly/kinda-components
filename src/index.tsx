52faa6a94a1c222b003ff094a06a114d46b4e480
Kinda Hall <1142704468@qq.com>
2022-09-04 13:40:42 +0800

    chore: 初始化项目整体框架并且安装部分依赖

---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+console.log(2);
+if (process.env.NODE_ENV !== 'production') {
+	console.log('Looks like we are in development mode!');
+}
---
+++ webpack/webpack.common.js
@@ -0,0 +1,75 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack'); // 用于访问内置插件
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+
+/**
+ * @type {import('webpack'.Configuration)}
+ * 补充webpack配置文件的vscode语法提示
+ */
+module.exports = {
+	target: 'web',
+	entry: {
+		main: path.resolve(__dirname, '../src/index.tsx'),
+	},
+	output: {
+		path: path.resolve(__dirname, '../dist'),
+		filename: '[name].[contenthash].bundle.js',
+	},
+	module: {
+		rules: [
+			{ test: /\.txt$/, use: 'raw-loader' },
+			{ test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+			{ test: /\.json$/, use: 'json-loader' },
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					// 将 JS 字符串生成为 style 节点
+					'style-loader',
+					// 将 CSS 转化成 CommonJS 模块
+					'css-loader',
+					// 自定义css文件前缀
+					'postcss-loader',
+					// 将 Sass 编译成 CSS
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.jsx$/,
+				use: [
+					'babel-loader'
+				]
+			},
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.less$/i,
+				use: [
+					// compiles Less to CSS
+					'style-loader',
+					'css-loader',
+					'postcss-loader',
+					'less-loader',
+				],
+			},
+		],
+	},
+	// optimization: {
+	// 	usedExports: true,
+	// },
+	plugins: [
+		new webpack.ProgressPlugin(),
+		// 插件自动生成html并挂载js文件
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, '../public/index.html'),
+			title: 'Kinda App',
+		}),
+		new CleanWebpackPlugin(),
+	],
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
+};
